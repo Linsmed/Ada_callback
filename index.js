37a6586ead6f7859922360c96abe2cbceaaa2012
Ibraheem <iejibode@gmail.com>
2023-01-28 11:43:14 +0100

    updated

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let setup = document.getElementById("setup");
 let btn1 = document.getElementById("btn1");
 let punchline = document.getElementsByClassName("punchline")[0];
+let jokeimg = document.getElementById("joke-img");
 let joke = {};
+
 const tellAJoke = () => {
   fetch("https://official-joke-api.appspot.com/random_joke ")
     .then((res) => res.json())
@@ -9,7 +11,10 @@ const tellAJoke = () => {
       (data) => (
         (setup.innerHTML = data.setup),
         (btn1.style.display = "block"),
-        (joke = data)
+        (jokeimg.src =
+          "https://cdn.pixabay.com/photo/2017/01/06/17/01/smiley-1958283__340.png")(
+          (joke = data)
+        )
       )
     )
     .catch((err) => console.log(err));
